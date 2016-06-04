8fdb9d0574ae2fa9e369dac444a02b2d797b7eef
Rafael Santos <rafaelnsantos1@gmail.com>
2016-06-04 15:57:40 -0300

    add pull to refresh nos favoritos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,11 +39,32 @@ angular.module('starter.controllers', [])
   
 })
 
-.controller('FavoritoCtrl', function($scope, Favoritos) {
+.controller('FavoritoCtrl', function($scope, Favoritos, MarcasOff, Marcas) {
   $scope.atualizar = function() {
     Favoritos.all().then(function(favoritos) {
       $scope.favoritos = favoritos;
     })
+  };
+  
+  //similar ao addFavorito do ProdutoCtrl
+  $scope.update = function() {
+    console.log($scope.favoritos.length);
+    var produto = $scope.favoritos;
+    for(var i = 0; i < $scope.favoritos.length; i++){
+      // MarcasOff.addFavorito(produto);
+      console.log(produto[i].id);
+      var id = produto[i].id;
+      Marcas.all(id).then(function(marcas){
+        var marca = marcas.data;
+        console.log(marca.length);
+        for(var j = 0; j < marca.length; j++){
+          MarcasOff.addMarca(marca[j]);
+          console.log(id);
+          MarcasOff.addRelacao([id, marca[j].marca.id, marca[j].observacao]);
+        }
+      });
+    }
+    $scope.$broadcast('scroll.refreshComplete');
   };
   
   $scope.atualizar();
